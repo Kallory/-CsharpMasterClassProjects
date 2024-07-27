e8e3a8867a580f9e99713de561f2a37fcf5835fd
Kallory <kory232323@gmail.com>
2024-07-27 09:18:47 -0500

    exception handling small exercise

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+public class InvalidTransactionException : Exception {
+    public TransactionData TransactionData { get; }
+
+    public InvalidTransactionException()
+    {
+        
+    }
+    public InvalidTransactionException(string message)
+        : base (message)
+    {
+        
+    }
+
+    public InvalidTransactionException(string message, Exception inner)
+        : base (message, inner)
+    {
+        
+    }
+
+    public InvalidTransactionException(string message, TransactionData transactionData)
+    {
+        
+    }
+
+    public InvalidTransactionException(string message, TransactionData transactionData, Exception inner)
+    {
+        
+    }
+}
+
+    public class TransactionData {
+        public string Sender { get; init; }
+        public string Receiver { get; init; }
+        public decimal Amount { get; init; }
+    }
+
