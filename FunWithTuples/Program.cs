9523ae6ed15d44a500c8885642b05559af18c043
Kallory <kory232323@gmail.com>
2024-08-01 18:44:56 -0500

    generics and tuples

--- FunWithTuples/Program.cs
+++ FunWithTuples/Program.cs
@@ -3,3 +3,13 @@
 Console.WriteLine(myTuple.Item1);
 
 Console.ReadLine();
+
+
+// This is how to do it without telling the class to have generics
+public static class TupleSwapExercise {
+    public static Tuple<T2, T1> SwapTupleItems<T1, T2>(Tuple<T1, T2> tuple) {
+
+
+        return new Tuple<T2, T1>(tuple.Item2, tuple.Item1);
+    }
+}
