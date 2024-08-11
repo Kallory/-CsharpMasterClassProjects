0da88db3be2d21c1aec9f420cc534e55cdd11a33
Kallory <kory232323@gmail.com>
2024-08-11 11:40:55 -0500

    creating dictionary that separates a list of players by team name

--- FunwithDictionaries/Program.cs
+++ FunwithDictionaries/Program.cs
@@ -1,4 +1,5 @@
 ﻿var playerTeamMapping = new Dictionary<string, string>();
+List<Player> playerList = new List<Player>();
 
 playerTeamMapping.Add("Lionel Messi", "ARG");
 playerTeamMapping.Add("Gerónimo Rulli", "ARG");
@@ -15,6 +16,52 @@ foreach (var playerTeamPair in playerTeamMapping) {
     Console.WriteLine($"Player: {playerTeamPair.Key}       Country: {playerTeamPair.Value}");
 }
 
+
+Player LionelMessi = new Player("Lionel Messi", "Captain", "ARG", 10000);
+Player GerónimoRulli = new Player("Gerónimo Rulli", "GK", "ARG", 9000);
+Player EmilianoMartínez = new Player("Emiliano Martínez", "GK", "ARG", 8000);
+Player ManuelNeuer = new Player("Manuel Neuer", "GK", "GER", 7000);
+Player OliverBaumann = new Player("Oliver Baumann", "GK", "GER", 9000);
+Player DavidOspina = new Player("David Ospina", "GK", "COL", 10000);
+Player CamiloVargas = new Player("Camilo Vargas", "GK", "COL", 5000);
+Player ÁlvaroMontero = new Player("Álvaro Montero", "GK", "COL", 8000);
+Player CarlosCuesta = new Player("Carlos Cuesta", "DF", "COL", 7000);
+Player JhonLucumí = new Player("Jhon Lucumí", "DF", "COL", 9000);
+
+playerList.Add(LionelMessi);
+playerList.Add(GerónimoRulli);
+playerList.Add(EmilianoMartínez);
+playerList.Add(ManuelNeuer);
+playerList.Add(OliverBaumann);
+playerList.Add(DavidOspina);
+playerList.Add(CamiloVargas);
+playerList.Add(ÁlvaroMontero);
+playerList.Add(CarlosCuesta);
+playerList.Add(JhonLucumí);
+
+CalculateAvgSalary(playerList);
+
+Dictionary<string, decimal> CalculateAvgSalary(IEnumerable<Player> playerList) {
+    // group players by team
+    var playersPerTeam = new Dictionary<string, List<Player>>();
+
+    // iterate through the collection of employees and add each of them to the list belonging to specific team
+    foreach (var player in playerList) {
+
+        // check if the team exists in the dictionary, if it doesn't, create a new list of Players and add a key value pair of TeamName and 
+        // said list to the dictionary
+        if (!playersPerTeam.ContainsKey(player.TeamName)) {
+            playersPerTeam[player.TeamName] = new List<Player>();
+        }
+
+        // playersPerTeam[player.TeamName] should return list of players on a specific team, so we add the player the list associated with the
+        // key, which in this case is a team name
+        playersPerTeam[player.TeamName].Add(player);
+    }
+
+    return null;
+}
+
 Console.WriteLine();
 Console.WriteLine("Press any key to exit.");
 Console.ReadKey();
@@ -24,3 +71,19 @@ enum FIFA_CODES {
     GER,
     COL
 };
+
+
+
+
+public class Player {
+    public string Name { get; private set; }
+    public string Position { get; private set; }
+    public string TeamName { get; private set; }
+    public decimal Salary { get; private set; }
+
+    public Player(string name, string position, string teamName, decimal salary) {
+        Name = name;
+        Position = position; TeamName = teamName;
+        Salary = salary;
+    }
+}
--- FunwithDictionaries/obj/Debug/net8.0/FunwithDictionaries.AssemblyInfo.cs
+++ FunwithDictionaries/obj/Debug/net8.0/FunwithDictionaries.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunwithDictionaries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83df07bb81b4c2939ddebed9953c246982f2c324")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb69dcd256401d553bdd03e9883d48415e4d4d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunwithDictionaries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunwithDictionaries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
