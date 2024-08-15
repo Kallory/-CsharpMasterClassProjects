e10e75fd2de5022e8e8b7213b4817036e8adbd04
Kallory <kory232323@gmail.com>
2024-08-15 14:29:06 -0500

    adding filtering options for list

--- StrategyPatternApplicationandMoreGenerics/Program.cs
+++ StrategyPatternApplicationandMoreGenerics/Program.cs
@@ -1,5 +1,21 @@
 ﻿var numbers = new List<int> { 10, 12, -100, 55, 17, 22};
 
+Console.WriteLine(@"Select filter:
+Even
+Odd");
+
+var userInput = Console.ReadLine();
+
+List<int> result;
+switch(userInput) {
+    case "Even":
+        result = SelectEven(numbers); break;
+    case "Odd":
+        result = SelectOdd(numbers); break;
+    default:
+        throw new NotSupportedException($"{userInput} is not a valid filter.");
+}
+
 Print(numbers);
 
 
